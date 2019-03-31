f8ce68fb131ff73851eb9d483b4b8e77cb2eaa33
Xi <xi@seanchenxi.com>
2019-03-31 23:52:39 +0200

    try async parsing

--- dist/index.js
+++ dist/index.js
@@ -16,14 +16,16 @@ var _options = _interopRequireDefault(require("./options.json"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function propertiesLoader(source) {
+  if (this.cacheable) {
+    this.cacheable();
+  }
+
   const options = (0, _loaderUtils.getOptions)(this) || {};
   (0, _schemaUtils.default)(_options.default, options, 'Properties Loader');
+  const callback = this.async();
 
-  const obj = _properties.default.parse(source, {
-    namespaces: options.style !== 'flatten',
-    include: true
+  _properties.default.parse(source, options, (err, result = {}) => {
+    const json = JSON.stringify(result).replace(/\u2028/g, '\\u2028').replace(/\u2029/g, '\\u2029');
+    callback(err, `export default ${json}`);
   });
-
-  const json = JSON.stringify(obj).replace(/\u2028/g, '\\u2028').replace(/\u2029/g, '\\u2029');
-  return `export default ${json}`;
 }
