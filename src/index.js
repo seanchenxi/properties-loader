47b4c80864e62d2ee678bf2282053298abf8df85
Xi <xi@seanchenxi.com>
2019-03-31 23:50:42 +0200

    try async parsing

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import {getOptions} from 'loader-utils';
+import { getOptions } from 'loader-utils';
 
 import validateOptions from 'schema-utils';
 
@@ -7,16 +7,18 @@ import Properties from 'properties';
 import schema from './options.json';
 
 export default function propertiesLoader(source) {
-  const options = getOptions(this) || {};
+  if (this.cacheable) {
+    this.cacheable();
+  }
 
+  const options = getOptions(this) || {};
   validateOptions(schema, options, 'Properties Loader');
 
-  const obj = Properties.parse(source, {
-    namespaces: options.style !== 'flatten',
-    include: true,
+  const callback = this.async();
+  Properties.parse(source, options, (err, result = {}) => {
+    const json = JSON.stringify(result)
+      .replace(/\u2028/g, '\\u2028')
+      .replace(/\u2029/g, '\\u2029');
+    callback(err, `export default ${json}`);
   });
-  const json = JSON.stringify(obj)
-    .replace(/\u2028/g, '\\u2028')
-    .replace(/\u2029/g, '\\u2029');
-  return `export default ${json}`;
 }
